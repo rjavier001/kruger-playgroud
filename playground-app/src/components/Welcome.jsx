d7136f17ad2888ef2f437b72dc0e6a06b1040ca0
Javier Vallejo <rjavier001@gmail.com>
2023-01-02 15:11:13 -0500

    Welcome created

--- playground-app/src/components/Welcome.jsx
+++ playground-app/src/components/Welcome.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Welcome = () => {
   return (
-    <div>Welcome</div>
+    <div className='Welcome__container'>
+      <h1>
+        Welcome to my Playground
+      </h1>
+      <p>
+        Hi Everyone!, my name is Javier Vallejo Molina and this is my playground, check some of my projects. If you like what you see don’t hesitate to contact me, I’ll be glad to help you
+      </p>
+    </div>
   )
 }
 
